3c550a65699e3656c468b28e2f85f648bcadc871
yujia.qin <yujia.qin@yitu-inc.com>
2018-11-28 10:47:55 +0800

    test

--- scripts/init-configserver.js
+++ scripts/init-configserver.js
@@ -4,8 +4,8 @@ rs.initiate(
       configsvr: true,
       version: 1,
       members: [
-         { _id: 0, host : "config01:27017" },
-         { _id: 1, host : "config02:27017" }
+         { _id: 0, host : "config01:37019" },
+         { _id: 1, host : "config02:37019" }
       ]
    }
 )
