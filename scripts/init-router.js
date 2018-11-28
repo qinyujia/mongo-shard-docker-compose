0a3ca53a8d6dd9aa6ea1397785be4736197a2e12
yujia.qin <yujia.qin@yitu-inc.com>
2018-11-28 09:26:40 +0800

    test

--- scripts/init-router.js
+++ scripts/init-router.js
@@ -1,8 +1,3 @@
-sh.addShard("shard01/shard01a:27018")
-sh.addShard("shard01/shard01b:27018")
+sh.addShard("shard01/shard01:27018")
 
-sh.addShard("shard02/shard02a:27019")
-sh.addShard("shard02/shard02b:27019")
-
-sh.addShard("shard03/shard03a:27020")
-sh.addShard("shard03/shard03b:27020")
+sh.addShard("shard02/shard02:27019")
--- scripts/init-shard01.js
+++ scripts/init-shard01.js
@@ -3,8 +3,7 @@ rs.initiate(
       _id: "shard01",
       version: 1,
       members: [
-         { _id: 0, host : "shard01a:27018" },
-         { _id: 1, host : "shard01b:27018" },
+         { _id: 0, host : "shard01:27018" },
       ]
    }
 )
