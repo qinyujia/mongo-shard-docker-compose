4fe28190056c626979cd5ad40d150ec406dcc541
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-10 20:21:40 +0800

    remove incorrect echo

--- init-database.sh
+++ init-database.sh
@@ -1,1 +1,2 @@
+echo "--------init database--------"
 docker exec mongos_router02_1 sh -c "mongo --port 27017 < /scripts/init-database.js"
--- scripts/init-database.js
+++ scripts/init-database.js
@@ -1,6 +1,3 @@
-echo "--------shard database--------"
 sh.enableSharding("viid")
-echo "--------create collection--------"
 db.createCollection("viid.imageInfo")
-echo "--------shard collection--------"
 sh.shardCollection("viid.imageInfo", { jpaShotTime: 1, jpaDeviceID: 1 })
