577b2eb8061648de1bbc599a3b045609c525672e
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-10 18:27:11 +0800

    remove multi shard cmd

--- scripts/init-database.js
+++ scripts/init-database.js
@@ -1,4 +1,3 @@
 sh.enableSharding("viid")
 db.createCollection("viid.imageInfo")
-sh.shardCollection("viid.imageInfo", {"_id" : 1})
 sh.shardCollection("viid.imageInfo", { jpaShotTime: 1, jpaDeviceID: 1 })
