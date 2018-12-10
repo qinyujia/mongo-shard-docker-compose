20979688a393ff180dbd1405d0bb963ec1a7c58e
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-10 20:17:19 +0800

    no message

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "--------init config server--------"
-docker exec mongos_config01_1 sh -c "mongo --port 27018 < /scripts/init-configserver.js"
+docker exec mongos_config02_1 sh -c "mongo --port 27018 < /scripts/init-configserver.js"
 echo "--------init shard--------"
-docker exec mongos_shard01_1 sh -c "mongo --port 27019 < /scripts/init-shard01.js"
+docker exec mongos_shard02_1 sh -c "mongo --port 27019 < /scripts/init-shard01.js"
 sleep 5
 echo "--------init router--------"
-docker exec mongos_router01_1 sh -c "mongo --port 27017 < /scripts/init-router.js"
+docker exec mongos_router02_1 sh -c "mongo --port 27017 < /scripts/init-router.js"
