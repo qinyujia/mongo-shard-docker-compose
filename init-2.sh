b69476504dd351cbe211aae471662091838a030e
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-10 20:03:42 +0800

    echo log

--- init-2.sh
+++ init-2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+echo "--------init config server--------"
 docker exec mongos_config02_1 sh -c "mongo --port 27018 < /scripts/init-configserver.js"
+echo "--------init shard--------"
 docker exec mongos_shard02_1 sh -c "mongo --port 27019 < /scripts/init-shard02.js"
 sleep 20
+echo "--------init router--------"
 docker exec mongos_router02_1 sh -c "mongo --port 27017 < /scripts/init-router.js"
--- scripts/init-database.js
+++ scripts/init-database.js
@@ -1,3 +1,6 @@
+echo "--------shard database--------"
 sh.enableSharding("viid")
+echo "--------create collection--------"
 db.createCollection("viid.imageInfo")
+echo "--------shard collection--------"
 sh.shardCollection("viid.imageInfo", { jpaShotTime: 1, jpaDeviceID: 1 })
