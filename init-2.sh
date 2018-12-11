78b8e3245d2babe1a2d3aa8a0e0dcecab8308579
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-11 16:34:03 +0800

    sleep 10

--- init-2.sh
+++ init-2.sh
@@ -3,6 +3,6 @@ echo "--------init config server--------"
 docker exec mongos_config02_1 sh -c "mongo --port 27018 < /scripts/init-configserver.js"
 echo "--------init shard--------"
 docker exec mongos_shard02_1 sh -c "mongo --port 27019 < /scripts/init-shard02.js"
-sleep 5
+sleep 10
 echo "--------init router--------"
 docker exec mongos_router02_1 sh -c "mongo --port 27017 < /scripts/init-router.js"
