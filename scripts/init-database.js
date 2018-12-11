670158a9168ccd8bfb571307e76ebe1fd8a58edd
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-11 17:04:18 +0800

    print

--- scripts/init-database.js
+++ scripts/init-database.js
@@ -1,15 +1,26 @@
+window.print("----start sharding viid---");
 sh.enableSharding("viid")
 sleep(5*1000)
+
+use viid
+window.print("----start createCollection imageInfo---");
 db.createCollection("viid.imageInfo")
 sleep(5*1000)
+window.print("----start shardCollection imageInfo---");
 sh.shardCollection("viid.imageInfo", { jpaShotTime: 1, jpaDeviceID: 1 })
 sleep(5*1000)
+
+window.print("----start createCollection face---");
 db.createCollection("viid.face")
 sleep(5*1000)
+window.print("----start shardCollection face---");
 sh.shardCollection("viid.face", { jpaShotTime: 1, jpaDeviceID: 1 })
 sleep(5*1000)
+
+window.print("----start createCollection motorVehicle---");
 db.createCollection("viid.motorVehicle")
 sleep(5*1000)
+window.print("----start shardCollection motorVehicle---");
 sh.shardCollection("viid.motorVehicle", { jpaShotTime: 1, jpaDeviceID: 1 })
 db.printShardingStatus()
 
