461d51f5e74cd39a53eeca765ce82355fe4dced0
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-11 11:55:33 +0800

    remove duplex

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "--------init config server--------"
-docker exec mongos_config02_1 sh -c "mongo --port 27018 < /scripts/init-configserver.js"
+#echo "--------init config server--------"
+#docker exec mongos_config02_1 sh -c "mongo --port 27018 < /scripts/init-configserver.js"
 echo "--------init shard--------"
 docker exec mongos_shard02_1 sh -c "mongo --port 27019 < /scripts/init-shard01.js"
-sleep 5
-echo "--------init router--------"
-docker exec mongos_router02_1 sh -c "mongo --port 27017 < /scripts/init-router.js"
+#sleep 5
+#echo "--------init router--------"
+#docker exec mongos_router02_1 sh -c "mongo --port 27017 < /scripts/init-router.js"
