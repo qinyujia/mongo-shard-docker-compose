d2ca007027e5a8fd5e07e5e748a02d174f4dbb36
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-11 16:53:55 +0800

    sleep

--- scripts/init-database.js
+++ scripts/init-database.js
@@ -1,8 +1,23 @@
 sh.enableSharding("viid")
+sleep(5*1000)
 db.createCollection("viid.imageInfo")
+sleep(5*1000)
 sh.shardCollection("viid.imageInfo", { jpaShotTime: 1, jpaDeviceID: 1 })
+sleep(5*1000)
 db.createCollection("viid.face")
+sleep(5*1000)
 sh.shardCollection("viid.face", { jpaShotTime: 1, jpaDeviceID: 1 })
+sleep(5*1000)
 db.createCollection("viid.motorVehicle")
+sleep(5*1000)
 sh.shardCollection("viid.motorVehicle", { jpaShotTime: 1, jpaDeviceID: 1 })
 db.printShardingStatus()
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
