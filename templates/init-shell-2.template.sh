206bde1d66c3a46cb70609857afbc50dd070a164
yujia.qin <yujia.qin@yitu-inc.com>
2018-12-19 18:22:01 +0800

    sudo

--- templates/init-shell-2.template.sh
+++ templates/init-shell-2.template.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "--------init config server--------"
-docker exec mongos_config${serverNo}_1 sh -c "mongo --port 27018 < /scripts/init-configserver.js"
+sudo docker exec mongos_config${serverNo}_1 sh -c "mongo --port 27018 < /scripts/init-configserver.js"
 echo "--------init shard--------"
-docker exec mongos_shard${serverNo}_1 sh -c "mongo --port 27019 < /scripts/init-shard.js"
+sudo docker exec mongos_shard${serverNo}_1 sh -c "mongo --port 27019 < /scripts/init-shard.js"
 sleep 20
 echo "--------init router--------"
-docker exec mongos_router${serverNo}_1 sh -c "mongo --port 27017 < /scripts/init-router.js"
+sudo docker exec mongos_router${serverNo}_1 sh -c "mongo --port 27017 < /scripts/init-router.js"
 sleep 10
 echo "--------init database--------"
-docker exec mongos_router${serverNo}_1 sh -c "mongo --port 27017 < /scripts/init-database.js"
+sudo docker exec mongos_router${serverNo}_1 sh -c "mongo --port 27017 < /scripts/init-database.js"
